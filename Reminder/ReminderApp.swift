790268bb40fa65aff7cfa41df2e3b30fb03205ce
Gurinder Singh <manivirk07@gmail.com>
2023-03-28 15:25:49 -0700

    Initial Commit

---
+++ Reminder/ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderApp.swift
+//  Reminder
+//
+//  Created by Gurinder Singh on 2023-03-28.
+//
+
+import SwiftUI
+
+@main
+struct ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
